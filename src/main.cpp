52b44ec87362370e037461239dd8daf334d09f8f
orenji-neko <markanthony.enfermo@gmail.com>
2023-05-31 22:46:08 +0800

    changing text

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@ int main() {
     while(!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("Hello World", 190, 200, 20, BLACK);
+            DrawText("the quick brown fox", 190, 200, 20, BLACK);
+            DrawText("jumps over the lazy dog", 190, 200, 20, BLACK);
         EndDrawing();
     }
     return 0;
